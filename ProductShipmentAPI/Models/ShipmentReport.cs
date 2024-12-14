353042e4d3e25bbe2e5c50f7f0e5735774610f32
Danil <d_silchenkov@mail.ru>
2024-12-15 02:37:11 +0300

    first commit

--- ProductShipmentAPI/Models/ShipmentReport.cs
+++ ProductShipmentAPI/Models/ShipmentReport.cs
@@ -1,13 +1,13 @@
-﻿namespace ProductShipmentAPI.Models
-{
-    public class ShipmentReport
+﻿    namespace ProductShipmentAPI.Models
     {
-        public string Name { get; set; }  // Название товара
-        public string Store { get; set; }  // Номер магазина
-        public DateTime ShipmentDate { get; set; }  // Дата отправления
-        public int TotalBatchSize { get; set; }  // Общий размер партии
-        public decimal TotalBatchCost { get; set; }  // Общая стоимость партии
-        public decimal TotalWeight { get; set; }  // Общий вес партии 
-    }
+        public class ShipmentReport
+        {
+            public string Name { get; set; }  // Название товара
+            public string Store { get; set; }  // Номер магазина
+            public DateTime ShipmentDate { get; set; }  // Дата отправления
+            public int TotalBatchSize { get; set; }  // Общий размер партии
+            public decimal TotalBatchCost { get; set; }  // Общая стоимость партии
+            public decimal TotalWeight { get; set; }  // Общий вес партии 
+        }
 
-}
+    }
